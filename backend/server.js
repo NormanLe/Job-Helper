b6ce63f937d48081dacee807f5fa63b07f79090f
normanle <norman.le@stonybrook.edu>
2019-11-20 20:53:15 -0500

    setup mongoose with mongodb

--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,19 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+require('dotenv').config();
 const app = express();
-const port = 3000;
-
 app.use(cors());
 app.use(express.json());
 
+const port = process.env.PORT;
+const atlas_uri = process.env.ATLAS_URI;
+
+mongoose.connect(atlas_uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
 
 const jobRouter = require('./routes/jobs');
 const userRouter = require('./routes/users');
 
 app.use('/jobs', jobRouter);
 app.use('/users', userRouter);
-
-app.listen(port, () => {
-    console.log(`Server is running on ${port}` );
-});
